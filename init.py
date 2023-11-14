c066b6349632a88a2bd2c5f266c875c268195a94
odin-the-ruler <somnathtiwari887@gmail.com>
2023-11-14 14:20:31 +0530

    Bot created and /start,/help,/rules,/ping commands are added.

---
+++ init.py
@@ -0,0 +1,71 @@
+import json
+# import telebot
+import os
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import requests
+
+
+
+BOT_TOKEN=os.environ['BOT_TOKEN']
+
+# with open('data.json') as f:
+#     cmd_data = json.load(f)
+
+
+# The URL of the JSON file
+url = "https://odin-the-ruler.github.io/english_grammar/grammar.json"
+
+# Send a GET request and get the data
+response = requests.get (url)
+
+# Parse the data as JSON
+cmd_data= response.json()
+
+def get_response(command):
+    # Check if the command is valid
+    if command in cmd_data:
+        # Return the value from the dictionary
+        return cmd_data[command]
+    else:
+        # Return a default message
+        return "Sorry, I don't understand that command."
+
+async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    command = update.message.text
+    # Get the response from the get_response function
+    response = get_response(command.split()[1].lower())
+    # Send the response to the user
+    await update.message.reply_text(response)
+
+async def help(update:Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("How to use the bot\n\n-   To use the bot, you need to type  /rules  followed by the name of the part of speech you want to learn about. For example,  /rules noun  will show you the rules for nouns.\n-   The bot will reply with a message that contains the definition, examples, and types of the part of speech you requested. It will also highlight the part of speech in the examples with bold text.\n-   You can also type  /rules all  to see a list of all the parts of speech that the bot can teach you.\n\nCommands\n_______________\n/rules partOfSpeach -> to get rules of a part of speach.\n/ping  -> to check my response time.")
+
+async def ping(update, context):
+    start = update.message.date
+    end = update.message.date
+    response_time = (end - start).total_seconds()
+    response_time_str = f"{response_time:.2f} seconds"
+    await update.message.reply_text(f"!Pong🏓\nResponse time: {response_time_str})")
+
+
+async def start(update:Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I will help you to revise your grammar rules 😊use /help to know how to use me")
+
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+# Command Handlers
+cmd_handler = CommandHandler("rules", rules)
+help_handler = CommandHandler("help",help)
+start_handler = CommandHandler("start",start)
+ping_handler = CommandHandler("ping",ping)
+
+# Adding command Handlers
+app.add_handler(cmd_handler)
+app.add_handler(help_handler)
+app.add_handler(start_handler)
+app.add_handler(ping_handler)
+
+app.run_polling()
---
+++ main.py
@@ -0,0 +1,45 @@
+import json
+import os
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN=os.environ['BOT_TOKEN']
+
+with open('data.json') as f:
+    cmd_data = json.load(f)
+
+
+def get_response(command):
+    # Check if the command is valid
+    if command in cmd_data:
+        # Return the value from the dictionary
+        return cmd_data[command]
+    else:
+        # Return a default message
+        return "Sorry, I don't understand that command."
+
+async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    command = update.message.text
+    # Get the response from the get_response function
+    response = get_response(command.split()[1])
+    # Send the response to the user
+    await update.message.reply_text(response)
+
+async def help(update:Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("use it like  */rules #partOfSPeach*\nEx-\n*/rules verb* or */rules modals*\nit will send all the rules of that perticular part of speach", parse_mode='MarkdownV2')
+
+
+
+async def start(update:Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I will help you to revise your grammar rules 😊use /help to know how to use me")
+
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+cmd_handler = CommandHandler("rules", rules)
+help_handler = CommandHandler("help",help)
+start_handler = CommandHandler("start",start)
+app.add_handler(cmd_handler)
+app.add_handler(help_handler)
+app.add_handler(start_handler)
+app.run_polling()
